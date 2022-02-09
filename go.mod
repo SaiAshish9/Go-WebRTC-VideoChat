77ca94b6f202a95aee2fb3b126fb064d78bf6809
saiashish9 <saiashish3760@gmail.com>
2022-02-09 13:10:08 +0530

    updated routes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module video-chat-app
 
 go 1.17
+
+require github.com/gorilla/websocket v1.4.2
--- server/rooms.go
+++ server/rooms.go
@@ -1,9 +1,51 @@
 package server
 
-// "github.com/gorilla/websocket"
-// "encoding/json"
-// "log"
-// "sunc"
+import (
+	"sync"
+	"time"
+
+	"math/rand"
+
+	"github.com/gorilla/websocket"
+	// "encoding/json"
+	// "log"
+)
+
+type Participant struct {
+	Host bool
+	Conn *websocket.Conn
+}
 
 type RoomMap struct {
+	Mutex sync.RWMutex
+	Map   map[string][]Participant
+}
+
+func (r *RoomMap) Init() {
+	r.Map = make(map[string][]Participant)
+}
+
+func (r *RoomMap) Get(roomID string) []Participant {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	return r.Map[roomID]
+}
+
+func (r *RoomMap) CreateRoom() string {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	rand.Seed(time.Now().UnixNano())
+	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	b := make([]rune, 8)
+
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+}
+
+func (r *RoomMap) DeleteRoom() {
+
 }
